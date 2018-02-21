6e9e0a9799055fd3ec790537f2f6379b9a445767
André Ferreira <contato@andreferreira.me>
2018-02-21 11:17:15 -0300

    Implementing Proof-of-Work in Javascript

--- main.js
+++ main.js
@@ -7,16 +7,27 @@ class Block {
     this.data = data
     this.previousHash = previousHash
     this.hash = this.calculateHash()
+    this.nonce = 0
   }
 
   calculateHash () {
-    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString()
+    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString()
+  }
+
+  mineBlock (difficulty) {
+    while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join('0')) {
+      this.nonce++
+      this.hash = this.calculateHash()
+    }
+
+    console.log('Block mined: ' + this.hash)
   }
 }
 
 class Blockchain {
   constructor () {
     this.chain = [this.createGenesisHash()]
+    this.difficulty = 5
   }
 
   createGenesisHash () {
@@ -29,7 +40,7 @@ class Blockchain {
 
   addBlock (newBlock) {
     newBlock.previousHash = this.getLatestBlock().hash
-    newBlock.hash = newBlock.calculateHash()
+    newBlock.mineBlock(this.difficulty)
     this.chain.push(newBlock)
   }
 
@@ -53,14 +64,8 @@ class Blockchain {
 
 let fakeCoin = new Blockchain()
 
+console.log('Mining block 1...')
 fakeCoin.addBlock(new Block(1, '02/01/2018', {amount: 4}))
+
+console.log('Mining block 2...')
 fakeCoin.addBlock(new Block(2, '04/01/2018', {amount: 10}))
-
-console.log('Is blockchain valid? ', fakeCoin.isChainValid())
-
-fakeCoin.chain[1].data = { amount: 100 }
-fakeCoin.chain[1].hash = fakeCoin.chain[1].calculateHash()
-
-console.log('Is blockchain valid? ', fakeCoin.isChainValid())
-
-console.log(JSON.stringify(fakeCoin, null, 4))
